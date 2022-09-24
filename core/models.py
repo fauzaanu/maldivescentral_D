417c4d0044ce982aa42422662b8abe8b6cff411e
Fauzaan Gasim <86226565+fauzaanu@users.noreply.github.com>
2022-09-24 16:51:43 +0500

    fixes

--- core/models.py
+++ core/models.py
@@ -7,6 +7,7 @@ class Listing(models.Model):
     listing_name = models.CharField(max_length=100)
     listing_description = models.TextField(max_length=500)
     listing_seo_name = models.SlugField(unique=True, blank=False, editable=False)
+    listing_original_url = models.CharField(max_length=200, default="#")
 
     def save(self, *args, **kwargs):
         self.listing_seo_name = slugify(self.listing_name)
--- maldivescentral/settings.py
+++ maldivescentral/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ao&)@6^sdr-46!l*=vtup*um7()(arc_1cl7npm%rr=#taey)_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['139.59.88.141','127.0.0.1','maldivescentral.com']
 
 
 # Application definition
